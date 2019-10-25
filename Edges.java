54484af29c3ad423332b3f70fc18a291af7407a7
Nicholas Calleja <nick.calleja545@gmail.com>
2019-10-25 14:30:12 -0400

    Created getters and setters for the Edge between Cities

--- Edges.java
+++ Edges.java
@@ -1,9 +1,11 @@
-// This class will be an Edge between two points
+// This class will be an Edge between two Cities
 public class Edges {
 	
+	// Vars: Starting City, Destination City, and the Distance between the Cities
 	City here, there;
 	double dist;
 	
+	//  Constructor for intializing an Edge between two Cities
 	public Edges(City here, City there, double dist)	{
 		
 		this.here = here;
@@ -11,8 +13,40 @@ public class Edges {
 		this.dist = dist;
 	}
 	
+	// Returns the Distance between two Cities
 	public double getDist()	{
 		
 		return dist;
 	}
+	
+	// Returns the City at the start of the Edge
+	public City getStartingCity()	{
+		
+		return here;
+	}
+	
+	// Returns the City at the end of the Edge 
+	public City getDestinationCity()	{
+		
+		return there;
+	}
+	
+	// Sets the distance between the two Cities
+	public void setDist(double distance)	{
+		
+		dist = distance;
+	}
+	
+	// Sets the Beginning of the Edge
+	public void setStartingCity(City A)	{
+		
+		here = A;
+	}
+	
+	// Sets the Destination of the Edge
+	public void setDestinationCity(City B)	{
+		
+		there = B;
+	}
+	
 }
