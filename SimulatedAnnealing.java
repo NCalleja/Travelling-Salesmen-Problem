17a8ed02c9ad66bd2b7f33aa65927f1556158593
Nicholas Calleja <nick.calleja545@gmail.com>
2019-12-07 16:44:57 -0500

    Working on SimulatedAnnealing.java currently, while finishing methods in the ToolsTSP.java

--- SimulatedAnnealing.java
+++ SimulatedAnnealing.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 // Simulated Annealing
-public class SimulatedAnnealing implements Answer {
+public class SimulatedAnnealing extends ToolsTSP implements Answer {
 	
 	// Vars: The Optimal Path, The Map
 	private Tour optimalPath;
@@ -10,15 +10,53 @@ public class SimulatedAnnealing implements Answer {
 	// Constructor for Simulated Annealing, takes in a Map to create a Tour
 	public SimulatedAnnealing(MapInfo map)	{
 		
+		super(map);
 		this.map = map;
 	}
 	
-	
+	// isPathBad will decide if the Path is worth taking
+	public boolean isPathBad(Tour optimal, Tour current, double temp, int counter)	{
+		
+		// Random Number
+		double ran = Math.random();
+		
+		// Getting a value to compare
+		double diff = optimal.getDist() - current.getDist();
+		double val = Math.exp(diff / temp);
+		
+		// Comparing Value
+		return ran < val;
+	}
 	
 	@Override
 	public Tour ComputePath() {
 		
+		Tour currentPath = giveRandomTour();
+		Tour bestCurrentPath = currentPath;
 		
+		optimalPath = bestCurrentPath;
+		
+		int i = 0;
+		double temp = 10;
+		
+		while(temp > 0)	{
+			
+			if(bestCurrentPath.getDist() > currentPath.getDist())	{
+				
+				bestCurrentPath = currentPath;
+				
+				if(optimalPath.getDist() > bestCurrentPath.getDist())	{
+					
+					optimalPath = bestCurrentPath;
+				}
+			}
+			else if(isPathBad(bestCurrentPath, currentPath, temp, i))	{
+				
+				bestCurrentPath = currentPath;
+			}
+			
+			
+		}
 		
 		return null;
 	}
--- ToolsTSP.java
+++ ToolsTSP.java
@@ -19,7 +19,7 @@ public abstract class ToolsTSP implements Answer {
 		// List of Integers, Int Array size of the Map, and a List of Edges
 		List<Integer> cities = new ArrayList<Integer>();
 		List<Edges> edges = new ArrayList<Edges>();
-		int[] tour = new int[mapTool.n];
+		int[] tourRan = new int[mapTool.n];
 		
 		// Simply a Random Number
 		Random ran = new Random();
@@ -27,36 +27,50 @@ public abstract class ToolsTSP implements Answer {
 		// Making Cities Array List the same Size
 		for(int i = 0; i < mapTool.n; i++)	{
 			
+			// Add the City
 			cities.add(i);
 		}
 		
 		// Randomly creating a list of ints
 		for(int i = mapTool.n; i > 0; i--)	{
 			
+			// Create the Random Tour
 			int deleted = cities.remove(ran.nextInt(i));
-			tour[i - 1] = deleted;
+			tourRan[i - 1] = deleted;
 		}
 		
 		// Now creating edges from the random tour int array
 		for(int i = 0; i < mapTool.n; i++)	{
 			
-			City here = mapTool.Cities.get(tour[i]);
-			City there = mapTool.Cities.get(tour[i + 1]);
+			City here = mapTool.Cities.get(tourRan[i]);
+			City there = mapTool.Cities.get(tourRan[i + 1]);
 			
-			double dist = mapTool.distances[tour[i]][tour[i + 1]];
+			double dist = mapTool.distances[tourRan[i]][tourRan[i + 1]];
 			
 			edges.add(new Edges(here, there, dist));
 		}
 		
 		// Vars are used to connect the last city to the starting city
-		City here = mapTool.Cities.get(tour[tour.length - 1]);
+		City here = mapTool.Cities.get(tourRan[tourRan.length - 1]);
 		City there = mapTool.Cities.get(0);
 		
-		double dist = mapTool.distances[tour[tour.length - 1]][tour[0]];
+		double dist = mapTool.distances[tourRan[tourRan.length - 1]][tourRan[0]];
 		
 		edges.add(new Edges(here, there, dist));
 		
 		// Return our new Random Tour
 		return new Tour(edges);
+	}
+	
+	/*
+	 * The 2-Opt heuristic is a operation to delete two of the edges in the path,
+	 * and re-connect them in the remaining possible ways. If the modified path is an
+	 * improvement over the previouw one, it becomes the best solution, otherwise it is discarded
+	 */
+	public Tour twoOptPath(Tour path)	{
+		
+		
+		
+		return null;
 	}
 }
